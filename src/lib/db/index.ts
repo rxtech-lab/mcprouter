2c6766ee1e3b3530a79e984bd22d31b6af604f1c
sirily11 <32106111+sirily11@users.noreply.github.com>
2025-09-14 16:40:47 +0800

    feat: add keys support in schema

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -3,13 +3,13 @@ import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
 
 const databaseUrl =
-  process.env.NODE_ENV === "test"
+  process.env.IS_PLAYWRIGHT_TEST === "true"
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL;
 
 if (!databaseUrl) {
   throw new Error(
-    `${process.env.NODE_ENV === "test" ? "TEST_DATABASE_URL" : "DATABASE_URL"} is not set`,
+    `${process.env.IS_PLAYWRIGHT_TEST === "true" ? "TEST_DATABASE_URL" : "DATABASE_URL"} is not set`
   );
 }
 
